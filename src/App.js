26f28a1ae2000cbc056b597555a701b3406fe6cc
伊達尚登 <naoto@MacBook-Air.local>
2020-07-23 22:47:11 +0900

    ボタンの機能を作った

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  function buttonFunc(text) {
+    alert(text);
+  }
+
   return (
     <div>
       <h1>React</h1>
       <p>gitの２回目のコミット</p>
+      <button onClick={() => buttonFunc("アラート")}>ボタン</button>
     </div>
   );
 }
